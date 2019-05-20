ac17620e522f4188916233d6881084e68ab4831a
Stig Melon Bratvold <stig.bratvold@gmail.com>
2019-05-20 17:31:52 +0200

    Attempt at adding Lottie

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('innsikt'), // Required
+  path: 'AntiDiffer_Shape_1.json', // Required
+  renderer: 'svg/canvas/html', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Innsikt", // Name for future reference. Optional.
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'AntiDiffer_Shape_1.json', // Required
+  renderer: 'svg/canvas/html', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Innsikt", // Name for future reference. Optional.
+})
