3f6cb0aa606e6fcf40d53f2291b6f61d43281ee8
Nils Magnus Englund <nme@stepone.no>
2019-05-20 18:52:30 +0200

    Added all three lottie animations

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById('innsikt'), // Required
-  path: 'AntiDiffer_Shape_1.json', // Required
-  renderer: 'svg/canvas/html', // Required
+  container: document.getElementById("innsikt"), // Required
+  path: "/AntiDiffer_Shape_1.json", // Required
+  renderer: "svg", // Required: svg/canvas/html
   loop: true, // Optional
   autoplay: true, // Optional
-  name: "Innsikt", // Name for future reference. Optional.
-})
+  name: "Innsikt" // Name for future reference. Optional.
+});
 
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  path: 'AntiDiffer_Shape_1.json', // Required
-  renderer: 'svg/canvas/html', // Required
+  container: document.getElementById("design"), // Required
+  path: "/AntiDiffer_Shape_2.json", // Required
+  renderer: "svg", // Required: svg/canvas/html
   loop: true, // Optional
   autoplay: true, // Optional
-  name: "Innsikt", // Name for future reference. Optional.
-})
+  name: "Design" // Name for future reference. Optional.
+});
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("teknologi"), // Required
+  path: "/AntiDiffer_Shape_3.json", // Required
+  renderer: "svg", // Required: svg/canvas/html
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Teknologi" // Name for future reference. Optional.
+});
